3b44cd75554ecaeb8a97c9cfb72ee808623e63a2
Alisa Treyfeld <alisa.tr@gmail.com>
2023-08-09 12:25:45 +0300

    Navbar: add hooks imports

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import localFonts from "next/font/local";
 import cn from "classnames";
