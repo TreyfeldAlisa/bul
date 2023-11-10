b4fba7e8525a199ee9eacbbacb3186ba9fcc0f21
Alisa Treyfeld <alisa.tr@gmail.com>
2023-11-10 15:15:19 +0300

    Update Background.jsx

--- components/Background/Background.jsx
+++ components/Background/Background.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import cn from "classnames";
 
 import styles from "./Background.module.css";
+import poster from "../../public/video/thumbnail.png";
 
 export default function Background(shape) {
-
     return (
         <div className={cn(styles.container)}>
             {shape.shape ? <div className={styles.shape} /> : null}
             <div className={styles.videoContainer}>
                 <video
+                    controls
                     autoPlay
-                    preload
+                    preload="none"
                     playsInline
                     loop
                     muted
                     className={cn(styles.video, "embed-responsive-item")}
                     src={require("../../public/video/ASCII.mp4")}
+                    poster={poster}
+                    lazyLoad
                 />
             </div>
         </div>
