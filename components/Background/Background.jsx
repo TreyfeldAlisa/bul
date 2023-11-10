315811653d14c5ec92a61c77571e513b1e182695
Alisa Treyfeld <alisa.tr@gmail.com>
2023-11-10 14:25:24 +0300

    video controls, lets talk paddings

--- components/Background/Background.jsx
+++ components/Background/Background.jsx
@@ -1,36 +1,26 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import cn from "classnames";
 
 import styles from "./Background.module.css";
 
 export default function Background(shape) {
 
-    const ref = useRef(null);
-
-    useEffect(() => {
-        ref &&
-            ref.current
-                .play()
-                .then(() => {})
-                .catch((err) => {
-                    // Video couldn't play, low power play button showing.
-                });
-    }, []);
-
     return (
         <div className={cn(styles.container)}>
             {shape.shape ? <div className={styles.shape} /> : null}
             <div className={styles.videoContainer}>
                 <video
-                    ref={ref}
-                    controls
                     autoPlay
+                    controls
+                    preload
                     playsInline
                     loop
                     muted
                     className={cn(styles.video, "embed-responsive-item")}
-                    src={require("../../public/video/ASCII.mp4")}
-                />{" "}
+                    // src={require("../../public/video/ASCII.mp4")}
+                >
+                    <source src="../../public/video/ASCII.mp4" type="video/mp4" />
+                </video>
             </div>
         </div>
     );
